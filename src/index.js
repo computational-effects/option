690c0bc805f784eb2b1efe3e731e56d27d48e2b0
joneshf <jones3.hardy@gmail.com>
2015-08-15 05:10:23 -0700

    Add `isNone`/`isSome`

--- src/index.js
+++ src/index.js
@@ -5,28 +5,39 @@ var sum = require('ce-sum');
 var mixin = require('ce-mixin').mixin;
 var Unit = require('ce-unit').Unit;
 
+var enumerableGet = pbp.enumerableGet;
 var enumerableValue = pbp.enumerableValue;
 var value = pbp.value;
 
-var None, Some;
-
 var Option = Object.create(sum.Either, {
   of: enumerableValue(Some),
+  isSome: enumerableGet(function() {
+    return false;
+  }),
+  isNone: enumerableGet(function() {
+    return false;
+  }),
 });
 
-None = mixin(sum.Left(Unit), Object.create(Option, {
+var None = mixin(sum.Left(Unit), Object.create(Option, {
   type: value('None'),
   args: value([]),
   ctor: value(function() {
     return None;
   }),
+  isNone: enumerableGet(function() {
+    return true;
+  }),
 }));
 
-Some = function(x) {
+function Some(x) {
   return mixin(sum.Right(x), Object.create(Option, {
     type: value('Some'),
     args: value([x]),
     ctor: value(Some),
+    isSome: enumerableGet(function() {
+      return true;
+    }),
   }));
 };
 
